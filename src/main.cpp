686e97155ded61aa08d8b7402aa4974cfbc83a2b
NatnaelTaddese <natnaeltaddese3@gmail.com>
2024-06-06 23:38:13 +0800

    added gtk support for native dialog windows

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,52 @@
-#include "appwindow.h"
 #include <opencv2/opencv.hpp>
 
-int main(int argc, char **argv)
+#include "appwindow.h"
+#include "utils/file_dialog.cpp"
+// #include <opencv2/opencv.hpp>
+
+// int main(int argc, char **argv)
+// {
+//     auto ui = AppWindow::create();
+//     cv::Mat image = cv::imread("test.jpg");
+
+//     ui->on_request_increase_value([&]
+//                                   { ui->set_counter(ui->get_counter() + 1); });
+
+//     ui->run();
+//     return 0;
+// }
+
+void process_image()
 {
-    auto ui = AppWindow::create();
-    cv::Mat image = cv::imread("test.jpg");
+    std::string file_path = showFileDialog();
+    if (!file_path.empty())
+    {
+        // Load the image with OpenCV
+        cv::Mat image = cv::imread(file_path);
+        if (image.empty())
+        {
+            std::cout << "Could not read the image: " << file_path << std::endl;
+        }
+        else
+        {
+            std::cout << "Image loaded: " << file_path << std::endl;
+            // appwindow->set_selected_image(file_path.c_str());
 
-    ui->on_request_increase_value([&]
-                                  { ui->set_counter(ui->get_counter() + 1); });
+            // Display the image using OpenCV (optional)
+            cv::imshow("Selected Image", image);
+            cv::waitKey(0);
+        }
+    }
+}
 
-    ui->run();
+int main()
+{
+    // Initialize Slint UI
+    auto appwindow = AppWindow::create();
+
+    // Connect button click signal
+    appwindow->on_clicked(process_image);
+
+    appwindow->run();
     return 0;
 }
---
+++ src/utils/file_dialog.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <gtk/gtk.h>
+
+// Function to show file dialog and get selected file path
+std::string showFileDialog()
+{
+    GtkWidget *dialog;
+    GtkFileChooserAction action = GTK_FILE_CHOOSER_ACTION_OPEN;
+    gint res;
+    std::string filename;
+
+    gtk_init(nullptr, nullptr);
+
+    dialog = gtk_file_chooser_dialog_new("Open File",
+                                         NULL,
+                                         action,
+                                         ("_Cancel"),
+                                         GTK_RESPONSE_CANCEL,
+                                         ("_Open"),
+                                         GTK_RESPONSE_ACCEPT,
+                                         NULL);
+
+    res = gtk_dialog_run(GTK_DIALOG(dialog));
+    if (res == GTK_RESPONSE_ACCEPT)
+    {
+        char *file_path;
+        GtkFileChooser *chooser = GTK_FILE_CHOOSER(dialog);
+        file_path = gtk_file_chooser_get_filename(chooser);
+        filename = file_path;
+        g_free(file_path);
+    }
+
+    gtk_widget_destroy(dialog);
+    while (gtk_events_pending())
+    {
+        gtk_main_iteration();
+    }
+
+    return filename;
+}
