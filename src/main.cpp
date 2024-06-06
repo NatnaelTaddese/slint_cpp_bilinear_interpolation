0126b81c0d2402aefb641f05bc99af08991f137f
NatnaelTaddese <natnaeltaddese3@gmail.com>
2024-06-06 22:05:28 +0800

    added opencv as a dependency for the build process

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include "appwindow.h"
+#include <opencv2/opencv.hpp>
 
 int main(int argc, char **argv)
 {
     auto ui = AppWindow::create();
+    cv::Mat image = cv::imread("test.jpg");
 
-    ui->on_request_increase_value([&]{
-        ui->set_counter(ui->get_counter() + 1);
-    });
+    ui->on_request_increase_value([&]
+                                  { ui->set_counter(ui->get_counter() + 1); });
 
     ui->run();
     return 0;
