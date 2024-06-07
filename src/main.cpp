fcb1b44e29c6b1a90f48215cd0554abfc9f7e4c7
NatnaelTaddese <natnaeltaddese3@gmail.com>
2024-06-07 11:12:47 +0800

    fixed image loading: slint expects an image objects as input property not an imageURL as a string 🫠

--- src/main.cpp
+++ src/main.cpp
@@ -46,7 +46,25 @@ int main()
     auto appwindow = AppWindow::create();
 
     // Connect button click signal
-    appwindow->on_clicked(load_image);
+
+    // appwindow->on_clicked(load_image);
+    appwindow->on_clicked([&]()
+                          {
+        std::string file_path = showFileDialog();
+        if (!file_path.empty()) {
+            // Load the image with OpenCV
+            cv::Mat image = cv::imread(file_path);
+            if (image.empty()) {
+                std::cout << "Could not read the image: " << file_path << std::endl;
+            } else {
+                std::cout << "Image loaded: " << file_path << std::endl;
+                appwindow->set_selected_image( slint::Image::load_from_path(file_path.c_str()));
+
+                // Display the image using OpenCV (optional)
+                // cv::imshow("Selected Image", image);
+                // cv::waitKey(0);
+            }
+        } });
 
     appwindow->run();
     return 0;
